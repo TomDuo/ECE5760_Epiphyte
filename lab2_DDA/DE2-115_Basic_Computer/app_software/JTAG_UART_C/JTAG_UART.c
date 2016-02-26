d3edbad320410fc4e300b78bb644ce91b7d57c49
carchard <cwa37@cornell.edu>
2016-02-26 16:39:55 -0500

    added code in the JTAG_UART example to read in special commands over UART and parse them into the 18bit floating point we are using in the DDA

--- lab2_DDA/DE2-115_Basic_Computer/app_software/JTAG_UART_C/JTAG_UART.c
+++ lab2_DDA/DE2-115_Basic_Computer/app_software/JTAG_UART_C/JTAG_UART.c
@@ -1,3 +1,11 @@
+#include <string.h>
+#include <stdlib.h>
+#include "../../BSP/system.h"
+#include <stdio.h>
+
+#include <stdint.h>
+#define FLOAT2_DDA_FIXED(t) (((uint32_t)((t) *(65536.0)))  & 0x03FFFF)
+
 /* function prototypes */
 void put_jtag(volatile int *, char);
 
@@ -15,14 +23,27 @@ int main(void)
 	   and store instructions will be used to access these pointer locations, 
 	   instead of regular memory loads and stores) */
 	volatile int * JTAG_UART_ptr 	= (int *) 0x10001000;	// JTAG UART address
+	char select_line = 0x0;
 
-	int data, i;
+	double tempFloat; //strtod returns a double
+	uint32_t dataLine;
+	int data, i, n;
+	int k1, k2, k13, kmid, x1, v1, x2, v2;
+	char command_index = 0;
 	char text_string[] = "\nJTAG UART example code\n> \0";
+	char command_string[20];
+	char k1_string[] = "k1:";
+	char k2_string[] = "k2:";
+	char kmid_string[] = "km:";
+	char k13_string[] = "k3:";
+	char x1_string[] = "x1:";
+	char v1_string[] = "v1:";
+	char x2_string[] = "x2:";
+	char v2_string[] = "v2:";
 
 	/* print a text string */
 	for (i = 0; text_string[i] != 0; ++i)
 		put_jtag (JTAG_UART_ptr, text_string[i]);
-
 	/* read and echo characters */
 	while(1)
 	{
@@ -31,6 +52,74 @@ int main(void)
 		{
 			data = data & 0x000000FF;			// the data is in the least significant byte
 			/* echo the character */
+
+			// Add the data to the current command string if not return
+			if ((data != '\0') && (command_index <19))
+			{
+				command_string[command_index] = data;
+				command_index++;
+			}
+			// clear the command string if there is a return
+			else
+			{
+				// check for a match on any of the special strings in the command string
+				if (strstr(command_string,k1_string) == command_string)
+				{
+					tempFloat = strtod(command_string + 3,NULL);
+					select_line = 0x1;
+
+				}
+				else if (strstr(command_string,k2_string) == command_string)
+				{
+					tempFloat = strtod(command_string + 3,NULL);
+					select_line = 0x2;
+				}
+				else if (strstr(command_string,k13_string) == command_string)
+				{
+					tempFloat = strtod(command_string + 3,NULL);
+					select_line = 0x3;
+				}
+
+				else if (strstr(command_string,kmid_string) == command_string)
+				{
+					tempFloat = strtod(command_string + 3,NULL);
+					select_line = 0x4;
+
+				}
+				else if (strstr(command_string,x1_string) == command_string)
+				{
+					tempFloat = strtod(command_string + 3,NULL);
+					select_line = 0x5;
+				}
+				else if (strstr(command_string,v1_string) == command_string)
+				{
+					tempFloat = strtod(command_string + 3,NULL);
+				select_line = 0x6;
+				}
+				else if (strstr(command_string,x2_string) == command_string)
+				{
+					tempFloat = strtod(command_string + 3,NULL);
+					select_line = 0x7;
+				}
+				else if (strstr(command_string,v2_string) == command_string)
+				{
+					tempFloat = strtod(command_string + 3,NULL);
+					select_line = 0x8;
+				}
+				else
+				{
+					select_line = 0x0;
+				}
+
+				dataLine = FLOAT2_DDA_FIXED(tempFloat);
+				// after reading a value, zero the index and clear the command string
+				command_index = 0;
+				for (n=0;n<20;n++){
+					command_string[n] = '\0';
+				}
+			}
+
+			
 			put_jtag (JTAG_UART_ptr, (char) data & 0xFF );
 		}
 	}
