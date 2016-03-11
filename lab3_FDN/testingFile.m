329a66af656d40191089ac52b37f91d736d2e053
Noah Levy <noahmslevy@gmail.com>
2016-03-11 14:56:18 -0500

    made testing file better to be able to compare cycle by cycle with the compNode

--- lab3_FDN/testingFile.m
+++ lab3_FDN/testingFile.m
@@ -1,7 +1,7 @@
 close all
 clear all
 
-eta = 0.005;
+eta = 0.0078;
 rho = 0.5;
 uN  = 0;
 uW  = 0;
@@ -11,14 +11,22 @@ numtSteps = 200;
 y = [];
 uPrev = 0;
 uCurrent = -1;
-
-for t=1:numtSteps    
+cycleNum = 0;
+k=input('\n next? [y/n]\n>','s');
+while (strcmp(k,'y'))  
     y = [y uCurrent];
+    cycleNum = cycleNum + 1
+    sumNeighbors = uN+uS+uE+uW-4*uCurrent
+    oneMinusEta = 1-eta
+    rhoMultSum = rho*sumNeighbors
+    rhoSumMultOneMinusEta = rhoMultSum * oneMinusEta
+    u = rhoSumMultOneMinusEta + uCurrent*2 - oneMinusEta*uPrev
     uNext = (1-eta)*(rho*(uN+uS+uE+uW-4*uCurrent))+2*uCurrent - (1-eta)*uPrev;
     uPrev = uCurrent;
     uCurrent = uNext;
+    k=input('\n next? [y/n]\n>','s');
 end
 
-figure(1)
-plot(1:numtSteps,y)
+%figure(1)
+%plot(1:numtSteps,y)
 
