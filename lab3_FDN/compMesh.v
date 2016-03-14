36731f7377d1061cb7766d82bf918f38a81e9076
Noah Levy <noahmslevy@gmail.com>
2016-03-14 12:21:49 -0400

    forgot to commit mesh last time

---
+++ lab3_FDN/compMesh.v
@@ -0,0 +1,56 @@
+module compMesh
+#(
+  parameter xSize = 10,
+  parameter ySize = 10
+)(
+
+  // Clocks and Resets
+    input clk,
+    input reset,
+
+  //Input Params
+  input signed [17:0] rho,
+  input signed [17:0] eta,
+  input [2:0] tensionSel,
+
+  // Output Values
+    output reg signed [17:0] out
+  );
+
+wire [17:0] mesh_u [0:ySize-1][0:xSize-1];
+reg signed [17:0] boundaryValue = 18'd0;
+wire [xSize*ySize-1:0] validOuts ;
+
+wire allValid = &validOuts;
+genvar x;
+genvar y;
+generate 
+    for(x=0; x<xSize; x=x+1) begin : xloop
+        for(y=0; y<ySize;y=y+1) begin : yloop
+            wire signed [17:0] uNorth = y != 0 ? mesh_u[y-1][x] : boundaryValue ;    
+            wire signed [17:0] uSouth = y != ySize-1 ? mesh_u[y+1][x] : boundaryValue ;    
+            wire signed [17:0] uWest = x != 0 ? mesh_u[y][x-1] : boundaryValue ;    
+            wire signed [17:0] uEast = x != xSize-1 ? mesh_u[y][x+1] : boundaryValue ;    
+            compNode #(x,y) cn (
+                .clk(clk),
+                .reset(reset),
+                .uNorth(uNorth),
+                .uSouth(uSouth),
+                .uEast(uEast),
+                .uWest(uWest),
+                .rho(rho),
+                .eta(eta),
+                .tensionSel(tensionSel),
+                .u(mesh_u[y][x]), 
+                .validOut(validOuts[xSize*y + x])
+            );
+        end
+    end
+endgenerate
+
+always @(posedge allValid) begin
+    out <= 18'd22;
+end
+
+endmodule
+
