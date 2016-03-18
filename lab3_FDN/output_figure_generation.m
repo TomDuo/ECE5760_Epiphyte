060d68d985e7a01586e8ffcdbe0de2ea9a66b0c3
Noah Levy <noahmslevy@gmail.com>
2016-03-18 16:34:31 -0400

    we audio 5000

--- lab3_FDN/output_figure_generation.m
+++ lab3_FDN/output_figure_generation.m
@@ -5,23 +5,28 @@
 % Recording and plotting output of audio codec
 
 close all
+clear
 clc
 
-recDuration = 5;
+recDuration = 3;
 inputDevice = 3;
 sampleRate  = 44100;
 nBits       = 16;
 nChannels   = 2;
-recObj = audiorecorder(sampleRate,nBits,nChannels,inputDevice);
+recObj = audiorecorder(sampleRate,nBits,nChannels);
 
-fprintf('Start Recording')
+fprintf('Start Recording\n')
 recordblocking(recObj,recDuration);
-fprintf('Done Recording')
+fprintf('Done Recording\n')
 
+figure(1);
 waveform = getaudiodata(recObj);
-
-figure(1)
-Pxx = pwelch(waveform);
+plot(waveform)
+figure(2)
+Fs = sampleRate
+Hs=spectrum.welch('Hamming',2048,50);
+psd(Hs,waveform,'Fs',Fs)
 title('Power Spectrum of Simulated Noise')
-xlable('Freq - Hz')
+set(gca,'xlim',[0 1])
+xlabel('Freq - Hz')
 ylabel('Power dB')
