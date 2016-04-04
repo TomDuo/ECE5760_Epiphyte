4290cf55752526150f145b916bc18d308a2e250e
Noah Levy <noahmslevy@gmail.com>
2016-04-04 13:58:57 -0400

    added fake hotter buffer (dizzy_buffer.v) for testing

---
+++ lab4_fract/dizzy_buffer.v
@@ -0,0 +1,36 @@
+module hotter_buffer (
+	data,
+	rdaddress,
+	rdclock,
+	wraddress,
+	wrclock,
+	wren,
+	q);
+
+	input	[2:0]  data;
+	input	[18:0]  rdaddress;
+	input	  rdclock;
+	input	[18:0]  wraddress;
+	input	  wrclock;
+	input	  wren;
+	output	[2:0]  q;
+
+    reg [2:0] mem_array [0:921600];
+    reg [2:0] qreg;
+
+    assign q = qreg;
+
+    always @(posedge wrclock)
+    begin
+        if (wren) 
+        begin
+        mem_array[wraddress] <= data;
+        end
+    end
+    always @(posedge rdclock)
+    begin
+        qreg <= mem_array[rdaddress];
+    end
+endmodule
+
+
