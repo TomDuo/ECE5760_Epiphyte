610b6d82720f958290b243809c52c63d26817a01
Noah Levy <nml45@cornell.edu>
2016-05-04 23:00:24 -0400

    log power estimator

--- lab6_mvis/LogPowerEstimator.m
+++ lab6_mvis/LogPowerEstimator.m
@@ -12,13 +12,15 @@ out= zeros(length(lb),length(x));
 abs_out = zeros(length(lb),length(x));
 lpf_abs_out = zeros(length(lb),length(x));
 env_out = zeros(length(lb),length(x));
-[lpf_b,lpf_a] = butter(2,60/fs);
 for i = 1:length(lb)
   [b,a] = butter(2,[lb(i)/fs, ub(i)/fs]);
+  [lpf_z, lpf_p, lpf_k] = butter(2,10/fs);
+  %lpf_k = lpf_k*(2^i);
+  [lpf_b,lpf_a] = zp2tf(lpf_z,lpf_p,lpf_k);
   out(i,:) = filter(b,a,x);
   abs_out(i,:) = abs(out(i,:));
   lpf_abs_out(i,:) = filter(lpf_b,lpf_a,abs_out(i,:).^2);
-  env_out(i,:) = out(i,:).*cos(j.*2*pi*lb(i).*t);%exp(j.*2*pi*lb(i).*t);
+  env_out(i,:) = out(i,:).*cos(2*pi*lb(i)/fs.*t);%exp(j.*2*pi*lb(i).*t);
   if MODE==1
   subplot(length(lb)/2,2,i);
   plot(t,out(i,:),t,abs_out(i,:),t,lpf_abs_out(i,:),t,env_out(i,:));
