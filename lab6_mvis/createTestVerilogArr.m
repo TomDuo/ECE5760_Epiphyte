4fa6c55254c1d359c86225d71059bd7afddec3a6
Noah Levy <nml45@cornell.edu>
2016-05-15 21:11:54 -0400

    onnor will not be dissapointed by me

---
+++ lab6_mvis/createTestVerilogArr.m
@@ -0,0 +1,22 @@
+clear;
+[x, fs] = audioread('transformers_cut.wav');
+
+ftarget = 1000;
+maxlen = 2e3;
+
+x = .9*sin(2*pi*ftarget/fs*1:maxlen);
+assert(fs==48e3,'We DUMB');
+
+x = (x*(2^15));
+x= round(x);
+fid = fopen('testVect.v','w');
+fprintf(fid, ['module testVect (\n' ...
+    'output reg signed [15:0] aud [0:%i]\n);\n\n'], maxlen-1);
+
+fprintf(fid,'initial begin\n');
+for n=0:maxlen-1
+    fprintf(fid,'aud[%i]=16''h%x;\n',n,typecast(int16(x(n+1)),'uint16'));
+end
+fprintf(fid,'\nend\n');
+fprintf(fid,'\n\nendmodule');
+fclose(fid);
---
+++ lab6_mvis/filter_tb.v
@@ -0,0 +1,146 @@
+//
+//thing
+`timescale 10ps/1ps
+
+module top;
+
+// Clocking
+
+localparam MAXLEN = 2000;
+reg reset = 1'b1;
+reg clk = 1;
+reg audclk = 1;
+reg enable = 0;
+reg signed [15:0] iAud_L;
+wire [15:0] oAud_10_30;
+wire [15:0] oAud_30_90;
+wire [15:0] oAud_90_270;
+wire [15:0] oAud_270_810;
+wire [15:0] oAud_810_2430;
+wire [15:0] oAud_2430_7290;
+wire [15:0] oAud_7290_21870;
+
+wire [10:0] pow_10_30;
+wire [10:0] pow_30_90;
+wire [10:0] pow_90_270;
+wire [10:0] pow_270_810;
+wire [10:0] pow_810_2430;
+wire [10:0] pow_2430_7290;
+wire [10:0] pow_7290_21870;
+
+
+
+
+
+
+
+
+
+
+always #5 clk = ~clk;
+always #35 audclk = ~audclk;
+
+integer n = 0;
+  wire signed [15:0] aud [0:MAXLEN-1];
+  testVect tv(
+      .aud(aud)
+  );
+  autoGen_BPF #(1) bpf10_30(
+      .clk(clk),
+      .aud_clk(audclk),
+      .reset(reset),
+      .enable(enable),
+      .iAud_L(iAud_L),
+
+      .oAud_L(oAud_10_30),
+      .power(pow_10_30)
+  );
+
+  autoGen_BPF #(2) bpf30_90(
+      .clk(clk),
+      .aud_clk(audclk),
+      .reset(reset),
+      .enable(enable),
+      .iAud_L(iAud_L),
+
+      .oAud_L(oAud_30_90),
+      .power(pow_30_90)
+  );
+
+  autoGen_BPF #(3) bpf_90_270(
+      .clk(clk),
+      .aud_clk(audclk),
+      .reset(reset),
+      .enable(enable),
+      .iAud_L(iAud_L),
+
+      .oAud_L(oAud_90_270),
+      .power(pow_90_270)
+  );
+
+  autoGen_BPF #(4) bpf270_810(
+      .clk(clk),
+      .aud_clk(audclk),
+      .reset(reset),
+      .enable(enable),
+      .iAud_L(iAud_L),
+
+      .oAud_L(oAud_270_810),
+      .power(pow_270_810)
+  );
+
+  autoGen_BPF #(5) bpf810_2430(
+      .clk(clk),
+      .aud_clk(audclk),
+      .reset(reset),
+      .enable(enable),
+      .iAud_L(iAud_L),
+
+      .oAud_L(oAud_810_2430),
+      .power(pow_810_2430)
+  );
+
+  autoGen_BPF #(6) bpf2430_7290(
+      .clk(clk),
+      .aud_clk(audclk),
+      .reset(reset),
+      .enable(enable),
+      .iAud_L(iAud_L),
+
+      .oAud_L(oAud_2430_7290),
+      .power(pow_2430_7290)
+  );
+  autoGen_BPF #(7) bpf7290_21870(
+      .clk(clk),
+      .aud_clk(audclk),
+      .reset(reset),
+      .enable(enable),
+      .iAud_L(iAud_L),
+
+      .oAud_L(oAud_7290_21870),
+      .power(pow_7290_21870)
+  );
+
+  initial begin
+
+      // Dump waveforms
+      $dumpfile("filterTest-sim.vcd");
+      $dumpvars;
+      #110;
+      reset = 1'b0;
+      iAud_L[15:0] <= aud[0];
+      #100;
+      enable = 1'b1;
+      repeat(2000) begin
+          #10;
+          if (n%7 == 0) begin
+              iAud_L[15:0] <= aud[n/7];
+              $display("%d",aud[n/7]);
+          end
+          n = n +1;
+      end
+      $finish;
+  end
+
+  endmodule
+
