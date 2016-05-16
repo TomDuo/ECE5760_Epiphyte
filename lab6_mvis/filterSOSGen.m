07482d721229897cee3715d1bc14159ee8132bc6
Noah Levy <noahmslevy@gmail.com>
2016-05-16 17:25:51 -0400

    Commit!

--- lab6_mvis/filterSOSGen.m
+++ lab6_mvis/filterSOSGen.m
@@ -9,9 +9,13 @@ fprintf('\n\n\nreg signed [26:0] sosMat [0:%i][0:5];\n\n',sosSize(1)-1);;
 
 fprintf('case(filterID)\n\n');
 
+sosMats = {1,length(lb)};
 for fIndex = 1:length(lb)
     fprintf('\n%i: begin\n\n',fIndex-1);
     [z,p,k] = butter(2,[lb(fIndex)/fs ub(fIndex)/fs]);
+    %[z,p,k] = cheby1(2,1,[lb(fIndex)/fs ub(fIndex)/fs]);
+    %[z,p,k] = ellip(2,1,30,[lb(fIndex)/fs ub(fIndex)/fs]);
+
     soshi = zp2sos(z,p,k);
 
     %plot(n,x,n,y,n,yf1,n,yf2);
@@ -23,5 +27,19 @@ for fIndex = 1:length(lb)
         end
     end
     fprintf('end\n');
+    sosMats{fIndex} = soshi;
 end
+
+
 fprintf('endcase');
+h=fvtool(sosMats{1},sosMats{2},sosMats{3},sosMats{4},sosMats{5},sosMats{6},sosMats{7});
+set(h,'DesignMask','off'); % Turn off design mask
+hchildren = get(h,'children');
+haxes = hchildren(strcmpi(get(hchildren,'type'),'axes'));
+hline = get(haxes,'children');
+set(hline,'linewidth',2)
+
+legend('340-612Hz','612-1102Hz','1102-1986Hz','1986-3570Hz','3570-6427Hz','6427-11568Hz','11568-20823Hz');
+xlim([0 20e3/48e3]);
+title('Logarithmically Spaced, 2nd order Band Pass Filters');
+
